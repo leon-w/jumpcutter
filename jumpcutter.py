1deb6a8c51b7fdf8c4b5a46a1bf79465d8e84c3c
Leon Wiemers <leonwiemers@gmail.com>
2020-12-25 17:29:51 +0100

    remove broken yt download feature

--- jumpcutter.py
+++ jumpcutter.py
@@ -11,8 +11,6 @@ import argparse
 from datetime import datetime
 import random
 
-import ytdl
-
 def getMaxVolume(s):
     maxv = float(np.max(s))
     minv = float(np.min(s))
@@ -60,7 +58,7 @@ def log(msg, box=False):
 start_time = datetime.now()
 
 parser = argparse.ArgumentParser(description='Modifies a video file to play at different speeds when there is sound vs. silence.')
-parser.add_argument('input_file', type=str,  help='the video file you want modified or a youtube url')
+parser.add_argument('input_file', type=str,  help='the video file you want modified')
 parser.add_argument('-o', '--output_file', type=str, default="", help="the output file. (optional. if not included, it'll just modify the input file name)")
 parser.add_argument('--silent_threshold', type=float, default=0.03, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume)")
 parser.add_argument('--sounded_speed', type=float, default=1.00, help="the speed that sounded (spoken) frames should be played at. Typically 1.")
@@ -91,15 +89,6 @@ NORMALIZE_AUDIO = args.normalize_audio
 TEMP_FOLDER = f"TEMP_{random.randint(10**8, 10**9 - 1)}"
 AUDIO_FADE_ENVELOPE_SIZE = 400 # smooth out transitiion's audio by quickly fading in/out (arbitrary magic number whatever)
 
-
-# check if we need to download the video first
-if ytdl.is_youtube_url(INPUT_FILE):
-    log(f"Downloading {INPUT_FILE}")
-    cb = None if args.silent else lambda x: print(f"Downloaded {x}%")
-    try:
-        INPUT_FILE = ytdl.download_video(INPUT_FILE, progress_callback=cb)
-    except:
-        error(f"Failed to download `{INPUT_FILE}`.")
 
 # check input file
 if not os.path.isfile(INPUT_FILE):
