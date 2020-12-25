98f182871d541a87f4ba31190a959062903cfebf
Leon Wiemers <leonwiemers@gmail.com>
2020-12-25 23:10:57 +0100

    add live watch mode

--- jumpcutter_fast.py
+++ jumpcutter_fast.py
@@ -4,6 +4,7 @@ import sys
 import os
 from datetime import datetime
 import argparse
+import atexit
 import numpy as np
 from scipy.io import wavfile
 import librosa
@@ -12,12 +13,18 @@ import librosa
 parser = argparse.ArgumentParser(description="Removes silence from a video")
 parser.add_argument("input_file", type=str, help="the video file you want modified")
 parser.add_argument("-db", type=float, default=40, help="db threshold of the parts to remove")
+parser.add_argument("-out_format", type=str, default="mp4", help="format of output file")
+parser.add_argument("-w", action="store_true", help="directly watch video with vlc")
+parser.add_argument("-vlc_path", type=str, default="/Applications/VLC.app/Contents/MacOS/VLC", help="path to vlc")
 
 args = parser.parse_args()
 
 salt = f"{random.randint(0, 10**5):05d}"
 audio_file = f"JC_AUDIO_{salt}.wav"
 script_file = f"JC_SCRIPT_{salt}.txt"
+
+if args.w:
+    args.out_format = "ts"
 
 
 def run_ffmpeg(cmd):
@@ -27,17 +34,21 @@ def run_ffmpeg(cmd):
         sys.exit(1)
 
 
-def appendToFileName(filename, extra):
-    dotIndex = filename.rfind(".")
-    return filename[:dotIndex] + extra + filename[dotIndex:]
+def cleanup():
+    try:
+        os.remove(audio_file)
+        os.remove(script_file)
+    except:
+        pass
 
+
+atexit.register(cleanup)
 
 start_time = datetime.now()
 
 # extract audio
 run_ffmpeg(["-i", args.input_file, "-acodec", "pcm_s16le", "-ac", "1", "-ar", "16000", audio_file])
 sample_rate, audio_data = wavfile.read(audio_file)
-os.remove(audio_file)
 
 # select sounded parts
 sounded_parts = librosa.effects.split(
@@ -63,7 +74,11 @@ with open(script_file, "w") as f:
 
 # render video
 frac = int((sounded_parts[:, 1] - sounded_parts[:, 0]).sum() / len(audio_data) * 100)
-out_file = appendToFileName(args.input_file, f" ({frac}%)")
+out_file = args.input_file.rsplit(".", 1)[0] + f" ({frac}%).{args.out_format}"
+
+if args.w:
+    subprocess.Popen([args.vlc_path, out_file, "--quiet"])
+
 run_ffmpeg(
     [
         "-i",
@@ -74,9 +89,11 @@ run_ffmpeg(
         "[vout]",
         "-map",
         "[aout]",
+        "-c:v",
+        "libx264",
         out_file,
+        "-y",
     ]
 )
-os.remove(script_file)
 
 print(f"Done. Saved to {out_file}. Time: {datetime.now() - start_time}")
